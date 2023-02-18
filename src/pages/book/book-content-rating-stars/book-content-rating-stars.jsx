bbae2a9a9154ad65aaa27f3f2fe6db5732700d0b
sergioivanov008 <sergioivanov008@gmail.com>
2023-02-19 02:08:16 +0300

    fix: fix key in BookContentRatingStars

--- src/pages/book/book-content-rating-stars/book-content-rating-stars.jsx
+++ src/pages/book/book-content-rating-stars/book-content-rating-stars.jsx
@@ -6,12 +6,18 @@ import './book-content-rating-stars.css';
 export const BookContentRatingStars = (props) => {
     const {rating, isNeedRating} = props;
     const curRating = rating && rating !== 0 && Math.floor(rating);
-    const workArr = [0, 0, 0, 0, 0].fill(1, 0, curRating);
+    const workArr = [0, 0, 0, 0, 0]
+        .fill(1, 0, curRating)
+        .map((el, index) => ({'id': index, 'value': el}));
 
     return (
     <div className='book-rating_full'>
             <div className='book-content-rating-stars'>
-                {workArr.map((el) => <img src={el === 1 ? fullStarImg : emptyStarImg} className='star' alt={el === 1 ? 'full star' : 'empty star'} />)}
+                {workArr.map((el) => (
+                    <img className='star'
+                        key={el.id}
+                        src={el.value === 1 ? fullStarImg : emptyStarImg}
+                        alt={el.value === 1 ? 'full star' : 'empty star'} />))}
             </div>
             {isNeedRating ? (
                 <div className='book-rating_score'>
