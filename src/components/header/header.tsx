c205d379bcac8fea55f7388823357c3403233660
sergioivanov008 <sergioivanov008@gmail.com>
2024-03-05 08:47:22 +0300

    refactor: refactor breadcrumb

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,27 +1,40 @@
 import React from 'react';
 import './header.css';
-import { Typography } from 'antd';
+import { Breadcrumb, Typography } from 'antd';
 import { HEADER_TEXT, ROUTE } from '@constants/constants';
 import { SettingOutlined } from '@ant-design/icons';
-import { useAppSelector } from '@hooks/typed-react-redux-hooks';
+import { useAppDispatch, useAppSelector } from '@hooks/typed-react-redux-hooks';
 import { useLocation } from 'react-router-dom';
+import { push } from 'redux-first-history';
 
 const { Title } = Typography;
 
 export const Header: React.FC = () => {
+    const dispatch = useAppDispatch();
     const location = useLocation();
     const collapsed = useAppSelector(state => state.aside.isAsideCollapsed);
     const spanClass = collapsed ? '' : 'span-block';
     const headerStyle = `header ${location.pathname === ROUTE.FEEDBACKS && 'feedback'}`;
+    const pathName = location.pathname;
 
     return (
         <header className={headerStyle}>
-                <div className='breadcrumb'>
-                    <div className="breadcrumb-item">
-                        {HEADER_TEXT.HEADER_TEXT_1}
-                    </div>
-                </div>
-            {location.pathname === ROUTE.MAIN &&
+            <Breadcrumb>
+                <Breadcrumb.Item
+                    className='breadcrumb-item'
+                    onClick={() => dispatch(push(ROUTE.MAIN))}
+                >
+                    Главная
+                </Breadcrumb.Item>
+                {pathName === ROUTE.FEEDBACKS &&
+                    <Breadcrumb.Item
+                        className='breadcrumb-item'
+                        onClick={() => dispatch(push(ROUTE.FEEDBACKS))}
+                    >
+                        Отзывы пользователей
+                    </Breadcrumb.Item>}
+            </Breadcrumb>
+            {pathName === ROUTE.MAIN &&
                 <div className="header-content">
                     <Title className='header-text'>
                         {HEADER_TEXT.HEADER_TEXT_2_1}
