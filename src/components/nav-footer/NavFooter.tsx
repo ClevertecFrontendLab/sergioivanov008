88f83c39b9a6f2dcdfb3275219c0316906ad70ac
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-05 23:25:48 +0300

    refactor(navfooter): change styles

--- src/components/nav-footer/NavFooter.tsx
+++ src/components/nav-footer/NavFooter.tsx
@@ -3,13 +3,21 @@ import { Flex, Text } from '@chakra-ui/react';
 import { ExitButton } from '~/components';
 import { NAV_FOOTER } from '~/constants';
 
-import s from './NavFooter.module.css';
-
 export function NavFooter() {
     return (
         <Flex gap={4} direction='column' align='flex-start' pl={6}>
-            <Text className={s.textLight}>{NAV_FOOTER.textLight}</Text>
-            <Text className={s.text}>{NAV_FOOTER.text}</Text>
+            <Text fontWeight={500} fontSize={12} lineHeight='133%' color='rgba(0, 0, 0, 0.24)'>
+                {NAV_FOOTER.textLight}
+            </Text>
+            <Text
+                fontWeight={400}
+                fontSize={12}
+                lineHeight='85%'
+                color='rgba(0, 0, 0, 0.64)'
+                whiteSpace='pre-line'
+            >
+                {NAV_FOOTER.text}
+            </Text>
             <ExitButton />
         </Flex>
     );
