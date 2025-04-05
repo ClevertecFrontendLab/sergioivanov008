e89d923a97bffb750f51270176daf7f9c6b1d209
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-05 23:36:26 +0300

    refactor(navcomponent): change styles

--- src/components/nav-component/NavComponent.tsx
+++ src/components/nav-component/NavComponent.tsx
@@ -3,11 +3,9 @@ import { Flex } from '@chakra-ui/react';
 import { Category } from '~/components';
 import { NAV_DATA } from '~/constants';
 
-import s from './NavComponent.module.css';
-
 export function NavComponent() {
     return (
-        <Flex direction='column' className={s.navComponent}>
+        <Flex direction='column' h='100%' overflowY='auto' pl='10px'>
             {NAV_DATA.map((el) => (
                 <Category key={el.id} data={el} />
             ))}
