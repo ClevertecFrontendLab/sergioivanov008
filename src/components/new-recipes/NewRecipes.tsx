c28f4265e654fe199a242f0efdcba7393e120f2f
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-05 21:43:28 +0300

    refactor(newrecipes): change styles

--- src/components/new-recipes/NewRecipes.tsx
+++ src/components/new-recipes/NewRecipes.tsx
@@ -4,13 +4,13 @@ import { Flex, IconButton, Text } from '@chakra-ui/react';
 import { SliderItem } from '~/components';
 import { SLIDER_DATA, TITLES } from '~/constants';
 
-import s from './NewRecipes.module.css';
-
 export function NewRecipes() {
     return (
         <Flex direction='column' gap={6}>
-            <Text className={s.text}>{TITLES.newRecipes}</Text>
-            <Flex gap={6} className={s.slider}>
+            <Text fontWeight={500} fontSize={48} lineHeight='100%'>
+                {TITLES.newRecipes}
+            </Text>
+            <Flex gap={6} position='relative'>
                 {SLIDER_DATA.map((el) => (
                     <SliderItem key={el.id} data={el} />
                 ))}
