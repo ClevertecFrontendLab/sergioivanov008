579637c42b13ccd070c073b871a0d59fdd74dd67
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-05 21:48:26 +0300

    refactor(mainheader): change styles

--- src/components/main-header/MainHeader.tsx
+++ src/components/main-header/MainHeader.tsx
@@ -1,14 +1,14 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, Heading } from '@chakra-ui/react';
 
 import { Filters } from '~/components';
 import { TITLES } from '~/constants';
 
-import s from './MainHeader.module.css';
-
 export function MainHeader() {
     return (
-        <Flex gap={8} direction='column' className={s.wrapper}>
-            <Text className={s.title}>{TITLES.bonAppetit}</Text>
+        <Flex gap={8} direction='column' align='center'>
+            <Heading fontWeight={700} fontSize={48} lineHeight='100%'>
+                {TITLES.bonAppetit}
+            </Heading>
             <Filters />
         </Flex>
     );
