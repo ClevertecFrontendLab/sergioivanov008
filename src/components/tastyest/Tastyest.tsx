18bf64648782be9aed8c63dc9d325a5fd1bc8823
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-05 21:38:45 +0300

    refactor(tastyest): change styles

--- src/components/tastyest/Tastyest.tsx
+++ src/components/tastyest/Tastyest.tsx
@@ -1,16 +1,16 @@
 import { ArrowForwardIcon } from '@chakra-ui/icons';
-import { Button, Flex, SimpleGrid, Text } from '@chakra-ui/react';
+import { Button, Flex, Heading, SimpleGrid } from '@chakra-ui/react';
 
 import { TastyestItem } from '~/components';
 import { BTN_TEXT, TASTYEST_DATA, TITLES } from '~/constants';
-
-import s from './Tastyest.module.css';
 
 export function Tastyest() {
     return (
         <Flex direction='column' gap={6}>
             <Flex justify='space-between' align='center'>
-                <Text className={s.text}>{TITLES.tastyest}</Text>
+                <Heading fontWeight={500} fontSize={48} lineHeight='100%'>
+                    {TITLES.tastyest}
+                </Heading>
                 <Button
                     rightIcon={<ArrowForwardIcon />}
                     colorScheme='myGreen'
