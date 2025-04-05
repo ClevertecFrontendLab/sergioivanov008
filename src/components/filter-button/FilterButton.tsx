9334eae77882e0c9f7436c8b214ea0a3dbb85510
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-05 23:32:46 +0300

    refactor(filterbutton): rewrite code

--- src/components/filter-button/FilterButton.tsx
+++ src/components/filter-button/FilterButton.tsx
@@ -1,13 +1,15 @@
-import { Flex } from '@chakra-ui/react';
+import { IconButton } from '@chakra-ui/react';
 
 import { CustomIcon24 } from '~/components';
 
-import s from './FilterButton.module.css';
-
 export function FilterButton() {
     return (
-        <Flex align='center' justify='center' className={s.button}>
-            <CustomIcon24 iconType='filter' props={{ boxSize: '24px' }} />
-        </Flex>
+        <IconButton
+            colorScheme='myBlack'
+            aria-label='new-recipes'
+            size='lg'
+            icon={<CustomIcon24 iconType='filter' props={{ boxSize: '32px' }} />}
+            variant='outline'
+        />
     );
 }
