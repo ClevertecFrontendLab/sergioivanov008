642454384f39e87261e7d400d8667a0635c320cc
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-05 23:49:00 +0300

    refactor(badge): change styles

--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -3,18 +3,19 @@ import { Flex, Image, Text } from '@chakra-ui/react';
 import { BADGE_STORE } from '~/constants';
 
 import { BadgePropsType } from '../types';
-import s from './Badge.module.css';
 
 export function Badge({ type, color }: BadgePropsType) {
     return (
-        <Flex align='center' gap={2} backgroundColor={color} className={s.badge}>
+        <Flex align='center' gap={2} backgroundColor={color} borderRadius={1} py='2px' px='8px'>
             <Image
                 boxSize='24px'
                 objectFit='cover'
                 src={BADGE_STORE[type].image}
                 alt={BADGE_STORE[type].category}
             />
-            <Text className={s.text}>{BADGE_STORE[type].category}</Text>
+            <Text fontWeight={400} fontSize={14} lineHeight='143%'>
+                {BADGE_STORE[type].category}
+            </Text>
         </Flex>
     );
 }
