56a39f9dcfb78bcf7303da8b642facbae8c4517c
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-06 04:48:39 +0300

    refactor(blogsitem): change styles

--- src/components/blogs-item/BlogsItem.tsx
+++ src/components/blogs-item/BlogsItem.tsx
@@ -6,11 +6,21 @@ import { BlogsItemPropsType } from '../types';
 
 export function BlogsItem({ data }: BlogsItemPropsType) {
     return (
-        <Card direction='row' overflow='hidden' variant='outline'>
-            <Stack py={5} px={4} gap={0}>
+        <Card
+            direction='row'
+            overflow='hidden'
+            variant='outline'
+            cursor='pointer'
+            transition='0.2s'
+            _hover={{
+                boxShadow:
+                    '0 2px 4px -1px rgba(32, 126, 0, 0.06), 0 4px 6px -1px rgba(32, 126, 0, 0.1)',
+            }}
+        >
+            <Stack pt='24px' pb='22px' px='24px' gap={0}>
                 <CardBody p={0} gap={7}>
                     <AvatarContent data={data.avatarData} />
-                    <Text py='2' noOfLines={3} fontWeight={400} fontSize={14} lineHeight='143%'>
+                    <Text mt='28px' noOfLines={3} fontWeight={400} fontSize={14} lineHeight='143%'>
                         {data.description}
                     </Text>
                 </CardBody>
