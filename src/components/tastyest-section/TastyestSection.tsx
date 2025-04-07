d3f6973b2e7e6407c5e1b65d5640daaa513c0824
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-07 12:49:26 +0300

    refactor(tastyestsection): change styles for adaptive

--- src/components/tastyest-section/TastyestSection.tsx
+++ src/components/tastyest-section/TastyestSection.tsx
@@ -1,13 +1,33 @@
 import { ArrowForwardIcon } from '@chakra-ui/icons';
-import { Button, Flex, Heading, SimpleGrid } from '@chakra-ui/react';
+import { Button, Grid, GridItem, Heading, SimpleGrid } from '@chakra-ui/react';
 
 import { RecipeItem } from '~/components';
 import { BTN_TEXT, TASTYEST_DATA, TITLES } from '~/constants';
 
 export function TastyestSection() {
     return (
-        <Flex direction='column' gap={6} mb='40px'>
-            <Flex justify='space-between' align='center'>
+        <Grid
+            mx='auto'
+            w='100%'
+            gap={{ base: '12px', lg: '16px', xl: '24px' }}
+            mb='40px'
+            templateAreas={{
+                base: `"one"
+                     "three"
+                     "two"`,
+                lg: `"one two"
+                     "three three"`,
+            }}
+            gridTemplateRows={{
+                base: 'auto auto auto',
+                lg: 'auto auto',
+            }}
+            gridTemplateColumns={{
+                base: '1fr',
+                lg: '1fr 1fr',
+            }}
+        >
+            <GridItem area='one'>
                 <Heading
                     fontWeight={{ base: '500' }}
                     fontSize={{ base: '24px', lg: '36px', xl: '48px' }}
@@ -15,6 +35,8 @@ export function TastyestSection() {
                 >
                     {TITLES.tastyest}
                 </Heading>
+            </GridItem>
+            <GridItem area='two' justifySelf={{ base: 'center', lg: 'flex-end' }}>
                 <Button
                     rightIcon={<ArrowForwardIcon />}
                     colorScheme='myGreen'
@@ -24,15 +46,17 @@ export function TastyestSection() {
                 >
                     {BTN_TEXT.allSelection}
                 </Button>
-            </Flex>
-            <SimpleGrid
-                columns={{ base: 1, md: 2, lg: 1, xl: 2 }}
-                spacing={{ base: '12px', md: '16px', xl: '24px' }}
-            >
-                {TASTYEST_DATA.map((el) => (
-                    <RecipeItem key={el.id} data={el} />
-                ))}
-            </SimpleGrid>
-        </Flex>
+            </GridItem>
+            <GridItem area='three'>
+                <SimpleGrid
+                    columns={{ base: 1, md: 2, lg: 1, xl: 2 }}
+                    spacing={{ base: '12px', md: '16px', xl: '24px' }}
+                >
+                    {TASTYEST_DATA.map((el) => (
+                        <RecipeItem key={el.id} data={el} />
+                    ))}
+                </SimpleGrid>
+            </GridItem>
+        </Grid>
     );
 }
