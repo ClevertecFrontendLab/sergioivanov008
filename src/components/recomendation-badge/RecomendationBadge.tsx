c147be2487f161d1cd5557df3885f79772d12521
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-07 11:56:33 +0300

    refactor(recomendationbadge): change style

--- src/components/recomendation-badge/RecomendationBadge.tsx
+++ src/components/recomendation-badge/RecomendationBadge.tsx
@@ -6,7 +6,7 @@ import { RecomendationBadgePropsType } from '../types';
 
 export function RecomendationBadge({ data }: RecomendationBadgePropsType) {
     return (
-        <Box position='absolute' bottom='20px' left='24px'>
+        <Box position='absolute' bottom='20px' left='24px' display={{ base: 'none', lg: 'block' }}>
             <Tag h='28px' bg='#d7ff94' px='8px' borderRadius='4px'>
                 <Avatar src={data.image} size='2xs' name={data.name} ml={-1} mr={2} />
                 <TagLabel>{`${data.name} ${TEXT.recommend}`}</TagLabel>
