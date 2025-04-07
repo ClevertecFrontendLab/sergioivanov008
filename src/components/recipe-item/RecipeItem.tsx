8720d8fc9ac9c4a42075f38e1546f14ce789e29e
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-07 12:32:37 +0300

    refactor(recipeitem): change styles for adaptive

--- src/components/recipe-item/RecipeItem.tsx
+++ src/components/recipe-item/RecipeItem.tsx
@@ -6,6 +6,7 @@ import {
     CardFooter,
     Flex,
     Heading,
+    IconButton,
     Image,
     Stack,
     Text,
@@ -30,14 +31,31 @@ export function RecipeItem({ data }: RecipeItemPropsType) {
             }}
         >
             <Box position='relative'>
-                <Image objectFit='cover' maxW='346px' src={data.image} alt={data.title} />
+                <Image
+                    objectFit='cover'
+                    maxW={{ base: '158px', lg: '346px' }}
+                    src={data.image}
+                    alt={data.title}
+                />
                 {data.recomendation && <RecomendationBadge data={data.recomendation} />}
             </Box>
 
-            <Stack py={5} px={4} gap={0}>
+            <Stack
+                pt={{ base: '8px', lg: '24px' }}
+                pb={{ base: '4px', lg: '20px' }}
+                px={{ base: '8px', lg: '24px' }}
+                gap={{ base: '20px', lg: '24px' }}
+            >
                 <CardBody p={0}>
-                    <Flex justify='space-between' pb='6'>
-                        <Badge type={data.badgeType} color='#ffffd3' />
+                    <Flex
+                        justify='space-between'
+                        align='center'
+                        h='24px'
+                        pb={{ base: '0px', lg: '24px' }}
+                    >
+                        <Box position={{ base: 'absolute', lg: 'relative' }} left='8px'>
+                            <Badge type={data.badgeType} color='#ffffd3' />
+                        </Box>
                         {data.recipeProps && (
                             <Flex gap={2}>
                                 {data.recipeProps.map((el) => (
@@ -51,10 +69,15 @@ export function RecipeItem({ data }: RecipeItemPropsType) {
                             </Flex>
                         )}
                     </Flex>
-                    <Heading size='md' noOfLines={1}>
+                    <Heading
+                        noOfLines={1}
+                        fontWeight={{ base: '500' }}
+                        fontSize={{ base: '16px', lg: '20px' }}
+                        lineHeight={{ base: '150%', lg: '140%' }}
+                    >
                         {data.title}
                     </Heading>
-                    <Text py='2' noOfLines={3}>
+                    <Text py='2' noOfLines={3} display={{ base: 'none', lg: 'block' }}>
                         {data.description}
                     </Text>
                 </CardBody>
@@ -64,11 +87,20 @@ export function RecipeItem({ data }: RecipeItemPropsType) {
                         leftIcon={<CustomIcon12 iconType='pin' />}
                         colorScheme='myBlack'
                         variant='outline'
-                        size='sm'
+                        size={{ base: 'xs', lg: 'sm' }}
+                        display={{ base: 'none', lg: 'block' }}
                     >
                         {BTN_TEXT.save}
                     </Button>
-                    <Button colorScheme='myBlack' variant='solid' size='sm'>
+                    <IconButton
+                        icon={<CustomIcon12 iconType='pin' />}
+                        colorScheme='myBlack'
+                        aria-label='new-recipes'
+                        variant='outline'
+                        size='xs'
+                        display={{ base: 'flex', lg: 'none' }}
+                    />
+                    <Button colorScheme='myBlack' variant='solid' size={{ base: 'xs', lg: 'sm' }}>
                         {BTN_TEXT.cook}
                     </Button>
                 </CardFooter>
