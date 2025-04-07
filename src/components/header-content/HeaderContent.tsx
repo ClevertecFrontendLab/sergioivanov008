321597d1d48a42ca12424ddda95ef3074c8c2a69
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-07 08:00:22 +0300

    refactor(headercontent): change styles for adaptive

--- src/components/header-content/HeaderContent.tsx
+++ src/components/header-content/HeaderContent.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 
 import { AvatarContent, BreadcrumbComponent, BurgerMenu, HeaderUserData } from '~/components';
 import { AVATAR_DATA } from '~/constants';
@@ -7,7 +7,9 @@ export function HeaderContent() {
     return (
         <Flex w='100%' alignItems='center' justify={{ base: 'flex-end', lg: 'space-between' }}>
             <BreadcrumbComponent />
-            <AvatarContent data={AVATAR_DATA} />
+            <Box display={{ base: 'none', lg: 'flex' }}>
+                <AvatarContent data={AVATAR_DATA} />
+            </Box>
             <Flex gap={8} display={{ base: 'flex', lg: 'none' }}>
                 <HeaderUserData />
                 <BurgerMenu />
