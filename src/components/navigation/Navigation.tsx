91b09c254ea1eea1eaccb7c088bc6885af344d2b
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-08 23:32:52 +0300

    refactor(navigation): change style

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -4,7 +4,7 @@ import { NavComponent, NavFooter } from '~/components';
 
 export function Navigation() {
     return (
-        <Flex direction='column' justify='space-between' h='100%'>
+        <Flex direction='column' justify='space-between' h='100%' gap='10px'>
             <NavComponent />
             <NavFooter />
         </Flex>
