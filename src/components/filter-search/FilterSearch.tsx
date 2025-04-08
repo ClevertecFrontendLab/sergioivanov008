082d0fb7eec0cba8c288bf4ab8b44f0f5b3beb04
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-08 14:59:14 +0300

    refactor(filtersearch): change style

--- src/components/filter-search/FilterSearch.tsx
+++ src/components/filter-search/FilterSearch.tsx
@@ -3,8 +3,8 @@ import { Input, InputGroup, InputRightElement } from '@chakra-ui/react';
 
 export function FilterSearch() {
     return (
-        <InputGroup>
-            <Input placeholder='Название или ингредиент...' size='lg' w='458px' />
+        <InputGroup size={{ base: 'sm', lg: 'lg' }} w={{ base: '284px', md: '404px', lg: '458px' }}>
+            <Input placeholder='Название или ингредиент...' />
             <InputRightElement>
                 <SearchIcon />
             </InputRightElement>
