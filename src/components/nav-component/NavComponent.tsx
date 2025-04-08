440f6d1fef7d9902fee99586e647b8b8685f6f52
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-08 23:34:24 +0300

    refactor(navcomponent): rewrite code - add chakra accordion

--- src/components/nav-component/NavComponent.tsx
+++ src/components/nav-component/NavComponent.tsx
@@ -1,14 +1,16 @@
-import { Flex } from '@chakra-ui/react';
+import { Accordion, Flex } from '@chakra-ui/react';
 
 import { Category } from '~/components';
 import { NAV_DATA } from '~/constants';
 
 export function NavComponent() {
     return (
-        <Flex direction='column' h='100%' overflowY='auto' pl='10px'>
-            {NAV_DATA.map((el) => (
-                <Category key={el.id} data={el} />
-            ))}
+        <Flex direction='column' h='100%' overflowY='auto' pl='10px' py='10px' pr='16px'>
+            <Accordion allowToggle>
+                {NAV_DATA.map((el) => (
+                    <Category key={el.id} data={el} />
+                ))}
+            </Accordion>
         </Flex>
     );
 }
