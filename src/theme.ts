8c82e4af843a5fb7831eac1db5a0f12ab61c91a7
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-08 15:53:11 +0300

    feat(app): add fonts to chakra

--- src/theme.ts
+++ src/theme.ts
@@ -26,7 +26,13 @@ const colors = {
     },
 };
 
+const fonts = {
+    heading: `'Inter', sans-serif`,
+    body: `'Inter', sans-serif`,
+};
+
 export const theme = extendTheme({
     breakpoints,
     colors,
+    fonts,
 });
