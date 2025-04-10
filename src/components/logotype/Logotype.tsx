754a32d246ccc520d1f93140e069111ac4ba8d40
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-10 22:54:35 +0300

    feat(logotype): add link to main page

--- src/components/logotype/Logotype.tsx
+++ src/components/logotype/Logotype.tsx
@@ -1,27 +1,30 @@
 import { Flex } from '@chakra-ui/react';
+import { NavLink } from 'react-router';
 
 import { LogoIcon, LogoText } from '~/components';
 
 export function Logotype() {
     return (
-        <Flex
-            gap={2}
-            width={{
-                base: '32px',
-                sm: '135px',
-                md: '308px',
-            }}
-            align='flex-end'
-        >
-            <LogoIcon boxSize={8} />
-            <LogoText
-                width='97px'
-                height='26px'
-                display={{
-                    base: 'none',
-                    md: 'block',
+        <NavLink to='/'>
+            <Flex
+                gap={2}
+                width={{
+                    base: '32px',
+                    sm: '135px',
+                    md: '308px',
                 }}
-            />
-        </Flex>
+                align='flex-end'
+            >
+                <LogoIcon boxSize={8} />
+                <LogoText
+                    width='97px'
+                    height='26px'
+                    display={{
+                        base: 'none',
+                        md: 'block',
+                    }}
+                />
+            </Flex>
+        </NavLink>
     );
 }
