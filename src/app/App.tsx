0488f3ff72ac1af6a045192c38b1d76f50b3b190
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-10 03:15:07 +0300

    feat(app): add route path to pagel

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 
 import { RootLayout } from '~/components';
-import { MainPage } from '~/pages';
+import { MainPage, Page } from '~/pages';
 
 function App() {
     return (
@@ -9,6 +9,7 @@ function App() {
             <Routes>
                 <Route path='/' element={<RootLayout />}>
                     <Route index element={<MainPage />} />
+                    <Route path='/:categoryId/:categoryItemId?' element={<Page />} />
                 </Route>
             </Routes>
         </BrowserRouter>
