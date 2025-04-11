d8ff1619423e5475c748654d14ff518f7bfcf1a0
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-11 06:15:38 +0300

    fix(app): add route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 
 import { RootLayout } from '~/components';
 import { MainPage, Page } from '~/pages';
@@ -10,6 +10,7 @@ function App() {
                 <Route path='/' element={<RootLayout />}>
                     <Route index element={<MainPage />} />
                     <Route path='/:categoryId/:categoryItemId?' element={<Page />} />
+                    <Route path='*' element={<Navigate to='/' replace />} />
                 </Route>
             </Routes>
         </BrowserRouter>
