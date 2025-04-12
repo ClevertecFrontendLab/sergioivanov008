4c0c53f544c5fecb0b0d68add18a0c94ea03df17
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-12 15:24:15 +0300

    refactor(filtersearch): change the placeholder color after review

--- src/components/filter-search/FilterSearch.tsx
+++ src/components/filter-search/FilterSearch.tsx
@@ -6,7 +6,11 @@ import { TEXT } from '~/constants';
 export function FilterSearch() {
     return (
         <InputGroup w={{ base: '284px', md: '404px', lg: '458px' }}>
-            <Input placeholder={TEXT.searchPlaceholder} size={{ base: 'sm', lg: 'lg' }} />
+            <Input
+                placeholder={TEXT.searchPlaceholder}
+                _placeholder={{ color: '#134b00' }}
+                size={{ base: 'sm', lg: 'lg' }}
+            />
             <InputRightElement boxSize={{ base: '32px', lg: '48px' }}>
                 <SearchIcon boxSize={{ base: '14px', lg: '18px' }} />
             </InputRightElement>
