77d0752910899ab262d09d10b477a72f3560d718
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-17 00:25:51 +0300

    fix(category): change item index in link

--- src/components/category/Category.tsx
+++ src/components/category/Category.tsx
@@ -26,7 +26,7 @@ export function Category({ data }: CategoryPropsType) {
                 _expanded={{ bg: '#eaffc7', fontWeight: 700 }}
                 _hover={{ bg: '#ffffd3' }}
                 as='span'
-                onClick={() => navigate(`/${data.categoryNav}/${data.items[2].id}`)}
+                onClick={() => navigate(`/${data.categoryNav}/${data.items[0].id}`)}
                 data-test-id={data.categoryNav}
             >
                 <Flex gap='12px' align='center'>
