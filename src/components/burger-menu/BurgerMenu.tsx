f77297f25c939ec7dcd69abf8ca33f6f66a434e0
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-17 21:22:51 +0300

    feat(burgermenu): add component

---
+++ src/components/burger-menu/BurgerMenu.tsx
@@ -0,0 +1,36 @@
+import { Box, Flex } from '@chakra-ui/react';
+
+import { BreadcrumbComponent, NavComponent, NavFooter } from '~/components';
+
+export function BurgerMenu() {
+    return (
+        <Box
+            position='fixed'
+            top='64px'
+            w='100vw'
+            h='100vh'
+            backdropFilter='blur(4px)'
+            bg='rgba(0, 0, 0, 0.16)'
+        >
+            <Box w='100vw' h='100vh' position='relative'>
+                <Flex
+                    position='absolute'
+                    top='0'
+                    right='8px'
+                    w='344px'
+                    h='calc(100vh - 60px - 84px)'
+                    bg='#fff'
+                    boxShadow='0 4px 6px -2px rgba(0, 0, 0, 0.05), 0 10px 15px -3px rgba(0, 0, 0, 0.1)'
+                    borderRadius='0 0 12px 12px'
+                    padding='16px 20px 32px'
+                    direction='column'
+                    gap='14px'
+                >
+                    <BreadcrumbComponent />
+                    <NavComponent />
+                    <NavFooter />
+                </Flex>
+            </Box>
+        </Box>
+    );
+}
