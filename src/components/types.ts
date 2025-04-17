bd84c0d7dcc7287b829b134066fc06916a302cca
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-18 00:36:35 +0300

    feat(constants): add mock data to vegan_recipes from task

--- src/components/types.ts
+++ src/components/types.ts
@@ -183,3 +183,35 @@ export type BreadcrumbSlotType = {
     text?: string;
     linkTo: string;
 };
+
+export type RecipeItemFullType = {
+    id: string;
+    title: string;
+    description: string;
+    category: string[];
+    subcategory: string[];
+    image: string;
+    bookmarks: number;
+    likes: number;
+    date: string;
+    time: string;
+    portions?: number;
+    nutritionValue: {
+        calories: number;
+        proteins: number;
+        fats: number;
+        carbohydrates: number;
+    };
+    ingredients: {
+        title: string;
+        count: string;
+        measureUnit: string;
+    }[];
+    steps: {
+        stepNumber: number;
+        description: string;
+        image: string;
+    }[];
+    meat?: string;
+    side?: string;
+};
