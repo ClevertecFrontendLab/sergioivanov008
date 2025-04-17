814a09b4186f32666abfbb17d34668bd7afdccef
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-17 21:25:47 +0300

    feat(burgermenubutton): add logic for close burger menu

--- src/components/burger-menu-button/BurgerMenuButton.tsx
+++ src/components/burger-menu-button/BurgerMenuButton.tsx
@@ -1,5 +1,6 @@
 import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons';
-import { Flex } from '@chakra-ui/react';
+import { Flex, useMediaQuery } from '@chakra-ui/react';
+import { useEffect } from 'react';
 
 import { useAppDispatch, useAppSelector } from '~/store/hooks';
 import { mainActions, mainSelector } from '~/store/slices/main-slice';
@@ -7,6 +8,16 @@ import { mainActions, mainSelector } from '~/store/slices/main-slice';
 export function BurgerMenuButton() {
     const { isOpenBurger } = useAppSelector(mainSelector);
     const dispatch = useAppDispatch();
+    const [isLargerThan1440] = useMediaQuery('(min-width: 1440px)');
+
+    useEffect(() => {
+        const closeBurgerMenu = () => {
+            dispatch(mainActions.setIsOpenBurger(false));
+            document.body.classList.remove('not-scrolled');
+        };
+
+        isLargerThan1440 && closeBurgerMenu();
+    }, [dispatch, isLargerThan1440]);
 
     const handlerBurger = () => {
         dispatch(mainActions.setIsOpenBurger(!isOpenBurger));
