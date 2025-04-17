b62aedbf28735177389eb892d3a297adc8f1eba2
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-17 18:10:08 +0300

    feat(burgermenu): add starting logic for open burger menu

--- src/components/burger-menu/BurgerMenu.tsx
+++ src/components/burger-menu/BurgerMenu.tsx
@@ -1,10 +1,25 @@
-import { HamburgerIcon } from '@chakra-ui/icons';
-import { Box } from '@chakra-ui/react';
+import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons';
+import { Flex } from '@chakra-ui/react';
+
+import { useAppDispatch, useAppSelector } from '~/store/hooks';
+import { mainActions, mainSelector } from '~/store/slices/main-slice';
 
 export function BurgerMenu() {
+    const { isOpenBurger } = useAppSelector(mainSelector);
+    const dispatch = useAppDispatch();
+
+    const handlerBurger = () => {
+        dispatch(mainActions.setIsOpenBurger(!isOpenBurger));
+        document.body.classList.toggle('not-scrolled');
+    };
+
     return (
-        <Box>
-            <HamburgerIcon boxSize={6} cursor='pointer' />
-        </Box>
+        <Flex boxSize={6} align='center' justify='center'>
+            {isOpenBurger ? (
+                <CloseIcon boxSize={3} cursor='pointer' onClick={handlerBurger} />
+            ) : (
+                <HamburgerIcon boxSize={6} cursor='pointer' onClick={handlerBurger} />
+            )}
+        </Flex>
     );
 }
