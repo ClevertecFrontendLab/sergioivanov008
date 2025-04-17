f94d805a457d63677d92c49c314389d86548d5b2
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-18 00:11:41 +0300

    feat(newrecipessection): add logic for swiper

--- src/components/new-recipes-section/NewRecipesSection.tsx
+++ src/components/new-recipes-section/NewRecipesSection.tsx
@@ -1,20 +1,51 @@
+import 'swiper/swiper-bundle.css';
+
 import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons';
 import { Box, Flex, IconButton } from '@chakra-ui/react';
+import { useRef } from 'react';
+import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react';
 
 import { RecipeItemLook, SectionTitle } from '~/components';
 import { NEW_RECIPES_DATA, TITLES } from '~/constants';
 
 export function NewRecipesSection() {
+    const swiperRef = useRef<SwiperRef | null>(null);
+
     return (
         <Flex direction='column' gap={{ base: '12px', xl: '24px' }} mb='40px'>
             <SectionTitle title={TITLES.newRecipes} />
             <Box position='relative'>
                 <Box overflow={{ base: 'unset', xl: 'hidden' }}>
-                    <Flex gap={{ base: '12px', xl: '24px' }}>
+                    <Swiper
+                        ref={swiperRef}
+                        loop={true}
+                        onSlideChange={() => console.log('slide change')}
+                        onSwiper={(swiper) => console.log(swiper)}
+                        breakpoints={{
+                            0: {
+                                slidesPerView: 2,
+                                spaceBetween: '12px',
+                            },
+                            768: {
+                                slidesPerView: 4,
+                                spaceBetween: '12px',
+                            },
+                            1440: {
+                                slidesPerView: 3,
+                                spaceBetween: '12px',
+                            },
+                            1920: {
+                                slidesPerView: 4,
+                                spaceBetween: '24px',
+                            },
+                        }}
+                    >
                         {NEW_RECIPES_DATA.map((el) => (
-                            <RecipeItemLook key={el.id} data={el} />
+                            <SwiperSlide key={el.id}>
+                                <RecipeItemLook data={el} />
+                            </SwiperSlide>
                         ))}
-                    </Flex>
+                    </Swiper>
                 </Box>
                 <IconButton
                     colorScheme='myBlack'
@@ -22,9 +53,11 @@ export function NewRecipesSection() {
                     size='lg'
                     icon={<ArrowBackIcon boxSize='32px' />}
                     position='absolute'
+                    zIndex={2}
                     top='147px'
                     left='-8px'
                     display={{ base: 'none', lg: 'block' }}
+                    onClick={() => swiperRef.current?.swiper.slidePrev()}
                 />
                 <IconButton
                     colorScheme='myBlack'
@@ -32,9 +65,11 @@ export function NewRecipesSection() {
                     size='lg'
                     icon={<ArrowForwardIcon boxSize='32px' />}
                     position='absolute'
+                    zIndex={2}
                     top='147px'
                     right='-8px'
                     display={{ base: 'none', lg: 'block' }}
+                    onClick={() => swiperRef.current?.swiper.slideNext()}
                 />
             </Box>
         </Flex>
