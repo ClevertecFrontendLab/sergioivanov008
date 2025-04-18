82f76a6f74e963d8cfd951281f992ddc57c2e3a3
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-19 01:56:05 +0300

    refactor(mainpage): add pagewrapper component

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,25 +1,20 @@
-import { Flex } from '@chakra-ui/react';
-
 import {
     BlogsSection,
     JuiciestSection,
     NewRecipesSection,
     PageFooterSection,
     PageHeaderSection,
+    PageWrapper,
 } from '~/components';
 
 export function MainPage() {
     return (
-        <Flex
-            direction='column'
-            pl={{ base: '16px', md: '20px', lg: '24px' }}
-            pr={{ base: '16px', md: '20px', lg: '24px' }}
-        >
+        <PageWrapper>
             <PageHeaderSection page='home' />
             <NewRecipesSection />
             <JuiciestSection />
             <BlogsSection />
             <PageFooterSection page='vegan' />
-        </Flex>
+        </PageWrapper>
     );
 }
