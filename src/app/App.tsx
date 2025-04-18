e85126d6b56d4ef9439aa9688c5ac1ffe9c1a83a
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-19 01:19:57 +0300

    feat(app): add route to recipepage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 
 import { RootLayout } from '~/components';
-import { MainPage, Page } from '~/pages';
+import { MainPage, Page, RecipePage } from '~/pages';
 
 function App() {
     return (
@@ -10,6 +10,7 @@ function App() {
                 <Route path='/' element={<RootLayout />}>
                     <Route index element={<MainPage />} />
                     <Route path='/:category/:subcategory?' element={<Page />} />
+                    <Route path='/:category/:subcategory/:id' element={<RecipePage />} />
                     <Route path='*' element={<Navigate to='/' replace />} />
                 </Route>
             </Routes>
