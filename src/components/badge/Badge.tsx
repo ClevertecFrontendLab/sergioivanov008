21f7658afa86d8b10a4ef15ece0f82dd029c601a
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-18 14:29:27 +0300

    fix(badge): rewrite component according new structure of mock data

--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -1,29 +1,44 @@
-import { Flex, Image, Text } from '@chakra-ui/react';
+import { Flex, Image, Stack, Text } from '@chakra-ui/react';
 
 import { CATEGORY_DB } from '~/constants';
 
-import { BadgePropsType } from '../types';
+import { BadgePropsType, CategoryKeyType } from '../types';
 
-export function Badge({ type, color }: BadgePropsType) {
+export function Badge({ category, color }: BadgePropsType) {
     return (
-        <Flex
-            align='center'
-            gap={{ base: '4px', lg: '8px' }}
-            backgroundColor={color}
-            borderRadius='4px'
-            py='2px'
-            px={{ base: '4px', lg: '8px' }}
-            h='24px'
-        >
-            <Image
-                boxSize='16px'
-                objectFit='cover'
-                src={CATEGORY_DB[type].image}
-                alt={CATEGORY_DB[type].category}
-            />
-            <Text fontWeight={400} fontSize={14} lineHeight='143%' whiteSpace='nowrap'>
-                {CATEGORY_DB[type].category}
-            </Text>
-        </Flex>
+        <Stack spacing='8px' align='flex-start' wrap='wrap'>
+            {category &&
+                category.map((el, index) => {
+                    const curItem = CATEGORY_DB[el as CategoryKeyType];
+
+                    return (
+                        <Flex
+                            align='center'
+                            gap={{ base: '4px', lg: '8px' }}
+                            backgroundColor={color}
+                            borderRadius='4px'
+                            py='2px'
+                            px={{ base: '4px', lg: '8px' }}
+                            h='24px'
+                            key={index}
+                        >
+                            <Image
+                                boxSize='16px'
+                                objectFit='cover'
+                                src={curItem.image}
+                                alt={curItem.category}
+                            />
+                            <Text
+                                fontWeight={400}
+                                fontSize={14}
+                                lineHeight='143%'
+                                whiteSpace='nowrap'
+                            >
+                                {curItem.category}
+                            </Text>
+                        </Flex>
+                    );
+                })}
+        </Stack>
     );
 }
