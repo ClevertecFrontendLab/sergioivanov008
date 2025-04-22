d31f84cf20a689e724bf2f1bd3fbc99abd15d716
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-22 09:50:13 +0300

    refactor(nutritioninfo): change style

--- src/components/nutrition-info/NutritionInfo.tsx
+++ src/components/nutrition-info/NutritionInfo.tsx
@@ -9,33 +9,40 @@ export function NutritionInfo({ data }: NutritionInfoPropsType) {
             <Text fontWeight={400} fontSize='14px' lineHeight='143%' color='rgba(0, 0, 0, 0.8)'>
                 {NUTRITION_INFO_TEXT.title}
             </Text>
-            <Flex direction='row' gap={{ base: '12px', xl: '24px' }}>
+            <Flex direction={{ base: 'column', md: 'row' }} gap={{ base: '12px', xl: '24px' }}>
                 {NUTRITION_INFO_TEXT.values.map((el) => (
                     <Card
                         key={el.id}
                         border='1px solid rgba(0, 0, 0, 0.08)'
                         borderRadius='16px'
                         p='16px'
-                        w={{ md: '173px', lg: '135px', xl: '149px' }}
-                        h='136px'
+                        w={{ base: '328px', md: '173px', lg: '135px', xl: '149px' }}
+                        h={{ base: '64px', md: '136px' }}
                     >
                         <CardBody p={0}>
-                            <Flex direction='column' gap='12px'>
+                            <Flex
+                                direction={{ base: 'row', md: 'column' }}
+                                gap='12px'
+                                justify={{ base: 'space-between', md: 'unset' }}
+                                align={{ base: 'center', md: 'unset' }}
+                            >
                                 <Text
                                     fontWeight={400}
                                     fontSize='14px'
                                     lineHeight='143%'
-                                    textAlign='center'
+                                    textAlign={{ base: 'unset', md: 'center' }}
                                     color='rgba(0, 0, 0, 0.48)'
+                                    w={{ base: '40%', md: 'unset' }}
                                 >
                                     {el.title}
                                 </Text>
                                 <Text
                                     fontWeight={500}
-                                    fontSize={{ md: '36px' }}
-                                    lineHeight={{ md: '111%' }}
+                                    fontSize={{ base: '24px', md: '36px' }}
+                                    lineHeight={{ base: '133%', md: '111%' }}
                                     textAlign='center'
                                     color='#134b00'
+                                    w={{ base: '30%', md: 'unset' }}
                                 >
                                     {data[el.key as NutritionValueKeyType]}
                                 </Text>
@@ -43,9 +50,10 @@ export function NutritionInfo({ data }: NutritionInfoPropsType) {
                                     fontWeight={600}
                                     fontSize={{ lg: '14px' }}
                                     lineHeight={{ lg: '143%' }}
-                                    textAlign='center'
+                                    textAlign={{ base: 'unset', md: 'center' }}
                                     color='rgba(0, 0, 0, 0.92)'
                                     textTransform='uppercase'
+                                    w={{ base: '20%', md: 'unset' }}
                                 >
                                     {el.dimension}
                                 </Text>
