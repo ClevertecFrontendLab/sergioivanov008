bc6b190dc377e4ea97e6ee7d07e21539b4c030aa
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-24 00:03:24 +0300

    feat(filterswitch): add starting logic

--- src/components/filter-switch/FilterSwitch.tsx
+++ src/components/filter-switch/FilterSwitch.tsx
@@ -1,14 +1,34 @@
 import { FormControl, FormLabel, Switch } from '@chakra-ui/react';
 
 import { TEXT } from '~/constants';
+import { useAppDispatch, useAppSelector } from '~/store/hooks';
+import { mainActions, mainSelector } from '~/store/slices/main-slice';
 
 export function FilterSwitch() {
+    const dispatch = useAppDispatch();
+    const { isExcludeAllergens } = useAppSelector(mainSelector);
+
+    const handlerSwitch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        dispatch(mainActions.setIsExcludeAllergens(e.target.checked));
+    };
+
     return (
-        <FormControl display='flex' alignItems='center' w='268px' justifyContent='space-between'>
-            <FormLabel htmlFor='email-alerts' mb='0'>
+        <FormControl
+            display='flex'
+            alignItems='center'
+            w='231px'
+            justifyContent='space-between'
+            pl='8px'
+        >
+            <FormLabel htmlFor='exclude-allergens' mb='0'>
                 {TEXT.switchPlaceholder}
             </FormLabel>
-            <Switch id='email-alerts' />
+            <Switch
+                id='exclude-allergens'
+                colorScheme='myGreen'
+                checked={isExcludeAllergens}
+                onChange={handlerSwitch}
+            />
         </FormControl>
     );
 }
