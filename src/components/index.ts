07e24294b2302f8b26285baa94c62ffb6f991da2
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-24 18:24:17 +0300

    feat(app): add import for component filterselectcustom

--- src/components/index.ts
+++ src/components/index.ts
@@ -19,6 +19,7 @@ export { ExitButton } from './exit-button/ExitButton.tsx';
 export { FilterButton } from './filter-button/FilterButton.tsx';
 export { FilterSearch } from './filter-search/FilterSearch.tsx';
 export { FilterSelect } from './filter-select/FilterSelect.tsx';
+export { FilterSelectCustom } from './filter-select-custom/FilterSelectCustom.tsx';
 export { FilterSwitch } from './filter-switch/FilterSwitch.tsx';
 export { Filters } from './filters/Filters.tsx';
 export { Footer } from './footer/Footer.tsx';
