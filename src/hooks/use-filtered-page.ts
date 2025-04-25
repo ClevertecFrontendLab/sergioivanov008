2ea900ed0fabf2aa729f13b1e0c51800b1243b5b
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-25 18:33:28 +0300

    feat(usefilteredpage): change logic in hook

--- src/hooks/use-filtered-page.ts
+++ src/hooks/use-filtered-page.ts
@@ -2,9 +2,12 @@ import { useAppSelector } from '~/store/hooks';
 import { mainSelector } from '~/store/slices/main-slice';
 
 export const useFilteredPage = () => {
-    const { isExcludeAllergens, isCanFiltered, selectedFilters } = useAppSelector(mainSelector);
+    const { isExcludeAllergens, isCanFiltered, selectedFilters, searchQuery } =
+        useAppSelector(mainSelector);
     const isShowFiltered =
-        (isExcludeAllergens && selectedFilters.allergens.length > 0) || isCanFiltered;
+        (isExcludeAllergens && selectedFilters.allergens.length > 0) ||
+        isCanFiltered ||
+        searchQuery;
 
     return isShowFiltered;
 };
