22c00cf2193d97bad6e26f9d09930d8712bc5a3c
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-25 18:34:05 +0300

    feat(filtersearch): add logic

--- src/components/filter-search/FilterSearch.tsx
+++ src/components/filter-search/FilterSearch.tsx
@@ -1,9 +1,29 @@
 import { SearchIcon } from '@chakra-ui/icons';
 import { Input, InputGroup, InputRightElement } from '@chakra-ui/react';
+import { useState } from 'react';
 
 import { TEXT } from '~/constants';
+import { useAppDispatch, useAppSelector } from '~/store/hooks';
+import { mainActions, mainSelector } from '~/store/slices/main-slice';
 
 export function FilterSearch() {
+    const dispatch = useAppDispatch();
+    const { searchQuery } = useAppSelector(mainSelector);
+    const [curQuery, setCurQuery] = useState(searchQuery);
+
+    const handleSetSerchQuery = () => {
+        const query = curQuery.trim();
+        if (query.length > 2) {
+            dispatch(mainActions.setSearchQuery(query));
+        }
+    };
+
+    const handleInputKeyDown = (e: React.KeyboardEvent) => {
+        if (e.key === 'Enter') {
+            handleSetSerchQuery();
+        }
+    };
+
     return (
         <InputGroup w={{ base: '284px', md: '404px', lg: '458px' }}>
             <Input
@@ -13,8 +33,11 @@ export function FilterSearch() {
                 borderRadius={{ base: '4px', lg: '6px' }}
                 border='1px solid rgba(0, 0, 0, 0.48)'
                 borderColor='rgba(0, 0, 0, 0.48)'
+                value={curQuery}
+                onChange={(e) => setCurQuery(e.target.value)}
+                onKeyDown={handleInputKeyDown}
             />
-            <InputRightElement boxSize={{ base: '32px', lg: '48px' }}>
+            <InputRightElement boxSize={{ base: '32px', lg: '48px' }} onClick={handleSetSerchQuery}>
                 <SearchIcon boxSize={{ base: '14px', lg: '18px' }} />
             </InputRightElement>
         </InputGroup>
