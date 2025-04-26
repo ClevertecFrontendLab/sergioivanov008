9959d5b8d9101261f28f2104066e4f6f5ed0325c
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-26 09:44:09 +0300

    feat(pagerecipessection): change source for data

--- src/components/page-recipes-section/PageRecipesSection.tsx
+++ src/components/page-recipes-section/PageRecipesSection.tsx
@@ -2,10 +2,12 @@ import { Button, Flex } from '@chakra-ui/react';
 
 import { RecipesGrid, RecipesTabs } from '~/components';
 import { PageRecipesSectionPropsType, PageType } from '~/components/types';
-import { BTN_TEXT, JUICIEST_DATA_FULL, VEGAN_DATA_FULL } from '~/constants';
+import { ALL_RECIPES, BTN_TEXT, VEGAN_DATA_FULL } from '~/constants';
+import { getSortByLikes } from '~/utils';
 
 export function PageRecipesSection({ page }: PageRecipesSectionPropsType) {
-    const curData = page === ('the-juiciest' as PageType) ? JUICIEST_DATA_FULL : VEGAN_DATA_FULL;
+    const curData =
+        page === ('the-juiciest' as PageType) ? getSortByLikes(ALL_RECIPES, 8) : VEGAN_DATA_FULL;
 
     return (
         <Flex direction='column' mb='40px' justify='center' align='center'>
