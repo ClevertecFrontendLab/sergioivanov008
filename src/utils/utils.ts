8088e608e1b2fc1357d1915791eeca173fd77ef0
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-26 09:28:57 +0300

    feat(util): add util getsortbylikes

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -11,4 +11,15 @@ export const getSortByDate = (arr: RecipeItemFullType[], num: number) => {
     return sortedArr.slice(0, num);
 };
 
+export const getSortByLikes = (arr: RecipeItemFullType[], num: number) => {
+    const sortedArr = arr.slice().sort((a, b) => {
+        if (!a.likes) return 1;
+        if (!b.likes) return -1;
+
+        return b.likes - a.likes;
+    });
+
+    return sortedArr.slice(0, num);
+};
+
 export const checkIsOdd = (num: number) => num % 2 === 0;
