f95d46ded95d195d63d42ebcab8522628d81f8e9
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-26 18:42:22 +0300

    feat(juiciestpage): rewrite code

--- src/pages/juiciest-page/JuiciestPage.tsx
+++ src/pages/juiciest-page/JuiciestPage.tsx
@@ -3,22 +3,29 @@ import { Divider } from '@chakra-ui/react';
 import {
     PageFooterSection,
     PageHeaderSection,
+    PageRecipesFilteredSection,
     PageRecipesSection,
     PageWrapper,
 } from '~/components';
 import { useFilteredPage } from '~/hooks/use-filtered-page';
+import { useAppSelector } from '~/store/hooks';
+import { mainSelector } from '~/store/slices/main-slice';
+import { getSortByLikes } from '~/utils';
 
 export function JuiciestPage() {
+    const { allRecipes } = useAppSelector(mainSelector);
     const isShowFilteredPage = useFilteredPage();
+
+    const curData = getSortByLikes(allRecipes, 8);
 
     return (
         <PageWrapper>
             <PageHeaderSection page='the-juiciest' />
             {isShowFilteredPage ? (
-                <PageRecipesSection page='filtered' />
+                <PageRecipesFilteredSection data={allRecipes} />
             ) : (
                 <>
-                    <PageRecipesSection page='the-juiciest' />
+                    <PageRecipesSection page='the-juiciest' data={curData} />
                     <Divider mb={{ base: '8px', lg: '24px' }} />
                     <PageFooterSection page='vegan' />
                 </>
