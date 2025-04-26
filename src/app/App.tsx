8931dc6f1d596beb479779be73d76f9bf0987a54
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-26 16:29:50 +0300

    feat(app): add route for juiciestpage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 
 import { RootLayout } from '~/components';
-import { MainPage, Page, RecipePage } from '~/pages';
+import { JuiciestPage, MainPage, Page, RecipePage } from '~/pages';
 
 function App() {
     return (
@@ -9,6 +9,7 @@ function App() {
             <Routes>
                 <Route path='/' element={<RootLayout />}>
                     <Route index element={<MainPage />} />
+                    <Route path='/the-juiciest' element={<JuiciestPage />} />
                     <Route path='/:category/:subcategory?' element={<Page />} />
                     <Route path='/:category/:subcategory/:id' element={<RecipePage />} />
                     <Route path='*' element={<Navigate to='/' replace />} />
