b124bc646f67f9c08cfe99e2f5777537c2c7da3f
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-27 19:17:54 +0300

    feat(categoryitem): add datatestid for tests

--- src/components/category-item/CategoryItem.tsx
+++ src/components/category-item/CategoryItem.tsx
@@ -1,14 +1,21 @@
 import { Box, Flex, Text } from '@chakra-ui/react';
-import { NavLink } from 'react-router';
+import { NavLink, useLocation } from 'react-router';
 
 import { CategoryItemPropsType } from '../types';
 import s from './CategoryItem.module.css';
 
 export function CategoryItem({ data, categoryNav }: CategoryItemPropsType) {
+    const location = useLocation();
+    const pathnameArr = location.pathname.split('/');
+    const category = pathnameArr[1];
+    const subcategory = pathnameArr[2];
+
     const linkTo = `/${categoryNav}/${data.subcategoryNav}`;
+    const isActiveItem = categoryNav === category && data.subcategoryNav === subcategory;
 
     return (
         <NavLink
+            data-test-id={isActiveItem ? `${subcategory}-active` : ''}
             to={linkTo}
             className={({ isActive }) => `${s.categoryItem} ${isActive ? s.active : ''}`}
         >
