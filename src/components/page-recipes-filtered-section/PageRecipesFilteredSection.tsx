3360ff1aee319ed85e3b3ee8aa78e79f2992f528
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-28 07:42:32 +0300

    feat(pagerecipesfilteredsection): add new logic

--- src/components/page-recipes-filtered-section/PageRecipesFilteredSection.tsx
+++ src/components/page-recipes-filtered-section/PageRecipesFilteredSection.tsx
@@ -1,13 +1,53 @@
+import { useEffect, useState } from 'react';
+
 import { PageRecipesSection } from '~/components';
-import { PageRecipesSectionPropsType } from '~/components/types';
+import { PageRecipesSectionPropsType, RecipeItemFullType } from '~/components/types';
 import { useAppSelector } from '~/store/hooks';
 import { mainSelector } from '~/store/slices/main-slice';
 
-export function PageRecipesFilteredSection({ page, data }: PageRecipesSectionPropsType) {
-    const { searchQuery } = useAppSelector(mainSelector);
-    const filteredData = data.filter((el) =>
-        el.title.toLowerCase().includes(searchQuery.toLowerCase()),
-    );
+export function PageRecipesFilteredSection({ data }: PageRecipesSectionPropsType) {
+    const { searchQuery, selectedFilters } = useAppSelector(mainSelector);
+    const [filteredData, setFilteredData] = useState<RecipeItemFullType[]>(data);
 
-    return <PageRecipesSection page={page} data={filteredData} />;
+    useEffect(() => {
+        let tempData: RecipeItemFullType[] = [];
+
+        if (searchQuery) {
+            tempData = data.filter((el) =>
+                el.title.toLowerCase().includes(searchQuery.toLowerCase()),
+            );
+        } else {
+            tempData = [...data];
+        }
+
+        if (selectedFilters.category.length > 0) {
+            tempData = tempData.filter((recipe) =>
+                recipe.category.some((category) =>
+                    selectedFilters.category.some((filter) => filter.key === category),
+                ),
+            );
+        }
+
+        if (selectedFilters.garnir.length > 0) {
+            tempData = tempData.filter((recipe) =>
+                selectedFilters.garnir.some((filter) => filter.key === recipe.side),
+            );
+        }
+
+        if (selectedFilters.allergens.length > 0) {
+            tempData = tempData.filter((recipe) => {
+                const containsAllergen = recipe.ingredients.some((ingredient) =>
+                    selectedFilters.allergens.some((allergen) =>
+                        ingredient.title.toLowerCase().includes(allergen.key.toLowerCase()),
+                    ),
+                );
+
+                return !containsAllergen;
+            });
+        }
+
+        setFilteredData(tempData);
+    }, [data, searchQuery, selectedFilters]);
+
+    return <PageRecipesSection data={filteredData} />;
 }
