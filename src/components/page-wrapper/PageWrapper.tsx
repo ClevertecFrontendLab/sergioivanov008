ea221639d4ae03fd2575d00c238eaf2a2518b19a
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-28 16:55:07 +0300

    refactor(pagewrapper): change style

--- src/components/page-wrapper/PageWrapper.tsx
+++ src/components/page-wrapper/PageWrapper.tsx
@@ -4,8 +4,8 @@ export function PageWrapper({ children }: { children: React.ReactNode }) {
     return (
         <Flex
             direction='column'
-            pl={{ base: '16px', md: '20px', lg: '24px' }}
-            pr={{ base: '16px', md: '20px', lg: '24px' }}
+            pl={{ base: '8px', md: '20px', lg: '24px' }}
+            pr={{ base: '8px', md: '20px', lg: '24px' }}
             minH='100vh'
         >
             {children}
