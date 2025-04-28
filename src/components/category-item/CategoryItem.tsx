5c095d9a59feb4dd5c1f5f4042017bd871f32802
sergioivanov008 <sergioivanov008@gmail.com>
2025-04-28 16:59:59 +0300

    feat(categoryitem): add logic

--- src/components/category-item/CategoryItem.tsx
+++ src/components/category-item/CategoryItem.tsx
@@ -1,10 +1,14 @@
 import { Box, Flex, Text } from '@chakra-ui/react';
 import { NavLink, useLocation } from 'react-router';
 
+import { useAppDispatch } from '~/store/hooks';
+import { mainActions } from '~/store/slices/main-slice';
+
 import { CategoryItemPropsType } from '../types';
 import s from './CategoryItem.module.css';
 
 export function CategoryItem({ data, categoryNav }: CategoryItemPropsType) {
+    const dispatch = useAppDispatch();
     const location = useLocation();
     const pathnameArr = location.pathname.split('/');
     const category = pathnameArr[1];
@@ -13,11 +17,17 @@ export function CategoryItem({ data, categoryNav }: CategoryItemPropsType) {
     const linkTo = `/${categoryNav}/${data.subcategoryNav}`;
     const isActiveItem = categoryNav === category && data.subcategoryNav === subcategory;
 
+    const nandlerMenuClose = () => {
+        dispatch(mainActions.setIsOpenBurger(false));
+        document.body.classList.remove('not-scrolled');
+    };
+
     return (
         <NavLink
             data-test-id={isActiveItem ? `${subcategory}-active` : ''}
             to={linkTo}
             className={({ isActive }) => `${s.categoryItem} ${isActive ? s.active : ''}`}
+            onClick={nandlerMenuClose}
         >
             <Flex gap={3} py={1.5} pr={2} pl={4} className={s.flex}>
                 <Flex boxSize={6} justify='flex-end'>
